92c5dc5b267c79c8847ebc2b7aab24dafd230e87
sahanbcs <sahanbcsrh@gmail.com>
2024-05-09 22:11:35 +0530

    language add

--- stock-wise-analytics-web/src/components/layout/partials/Header.tsx
+++ stock-wise-analytics-web/src/components/layout/partials/Header.tsx
@@ -49,6 +49,20 @@ function Header() {
                         <Link href="/contact">
                             <div className="w-[86px] h-[23px] text-center text-white text-base font-normal font-['Montserrat']">About Us</div>
                         </Link>
+                        <Link href="/language">
+                            <div className="flex items-center hover:text-gray-300 cursor-pointer">
+                                <div className="w-5 h-5 relative mr-3">
+                                    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g id="Globe vector">
+                                            <path id="Vector" d="M1 11C1 16.523 5.477 21 11 21C16.523 21 21 16.523 21 11C21 5.477 16.523 1 11 1C5.477 1 1 5.477 1 11Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                            <path id="Vector_2" d="M12 1.05005C12 1.05005 15 5.00005 15 11C15 17 12 20.9501 12 20.9501M10 20.9501C10 20.9501 7 17 7 11C7 5.00005 10 1.05005 10 1.05005M1.63 14.5H20.37M1.63 7.50005H20.37" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                        </g>
+                                    </svg>
+                                </div>
+                                <div className="text-center text-white text-base font-normal font-Montserrat">EN</div>
+                            </div>
+
+                        </Link>
                     </nav>
                     {/* Hamburger icon for mobile */}
                     <div className="flex">
